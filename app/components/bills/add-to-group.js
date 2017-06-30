9363e9714e184fb22139bdc7c4ca444d9386bf5b
Donald Wasserman <djwasserman@gmail.com>
2017-06-30 15:49:56 -0400

    added bills to a group

--- app/components/bills/add-to-group.js
+++ app/components/bills/add-to-group.js
@@ -5,6 +5,7 @@ const {inject: {service}, get, set} = Ember;
 
 export default Ember.Component.extend({
   store: service(),
+  currentUser: service(),
   classNames: ['w-100'],
   groupList: [],
   isActive: false,
@@ -12,6 +13,28 @@ export default Ember.Component.extend({
   listGroups: task(function * () {
     let groups = yield get(this, 'store').findAll('group');
     set(this, 'groupList', groups);
+  }),
+  addBillToGroup: task(function * (group) {
+    let {state_id} = get(this, 'bill');
+    let wrapper = yield this.get('store').query('wrapper', {bill__state_id: state_id});
+    let groupData = {group_id: group.id};
+
+    if (wrapper.get('length')) {
+      wrapper = wrapper.get('firstObject');
+      wrapper.get('groups').pushObject(groupData);
+
+    } else {
+      wrapper = this.get('store').createRecord('wrapper', {
+        bill: get(this, 'bill'),
+        groups: [groupData],
+        organization: get(this, 'currentUser.organization')
+      });
+    }
+    get(this, 'groupList').removeObject('groupList');
+    if (get(this, 'groupList').get('groupList') == 0) {
+      this.toggleProperty('isActive');
+    }
+    wrapper.save();
   }),
   actions: {
     toggleActive() {
--- app/models/wrapper.js
+++ app/models/wrapper.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  bill: DS.belongsTo('bill'),
+  groups: DS.attr(),
+  organization: DS.belongsTo('organization'),
+  notes: DS.attr()
 });
