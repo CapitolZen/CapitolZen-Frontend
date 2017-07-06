94c63dde52f32b2246024002ea327a66b0836267
Matt Wisner <matt@detroitweb.company>
2017-07-05 20:26:47 -0400

    Initing some changes

--- app/router.js
+++ app/router.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from "ember";
+import config from "./config/environment";
 
 const Router = Ember.Router.extend({
   location: config.locationType,
@@ -12,85 +12,87 @@ const Router = Ember.Router.extend({
   },
 
   _trackPage() {
-    Ember.run.scheduleOnce('afterRender', this, () => {
-      const page = this.get('url');
-      const title = this.getWithDefault('currentRouteName', 'unknown');
+    Ember.run.scheduleOnce("afterRender", this, () => {
+      const page = this.get("url");
+      const title = this.getWithDefault("currentRouteName", "unknown");
 
-      Ember.get(this, 'metrics').trackPage({ page, title });
+      Ember.get(this, "metrics").trackPage({ page, title });
     });
   }
-
 });
 
 Router.map(function() {
   //
   // Anon Pages
-  this.route('anon.login', {path: 'login'});
-  this.route('anon.register', {path: 'register'});
-  this.route('anon.forgot-password', {path: 'forgot-password'});
+  this.route("anon.login", { path: "login" });
+  this.route("anon.register", { path: "register" });
+  this.route("anon.forgot-password", { path: "forgot-password" });
 
-
-  this.route('app', {resetNamespace: true, path: ''}, function() {
-    this.route('dashboard', {resetNamespace: true, path: ''});
+  this.route("app", { resetNamespace: true, path: "" }, function() {
+    this.route("dashboard", { resetNamespace: true, path: "" });
 
     //
     // User Accounts
-    this.route('user', {resetNamespace: true}, function() {
-      this.route('edit');
+    this.route("user", { resetNamespace: true }, function() {
+      this.route("edit");
     });
 
     //
     // Organization Management
-    this.route('organization', {resetNamespace: true}, function() {
-      this.route('edit', {path: '/:id'});
+    this.route("organization", { resetNamespace: true }, function() {
+      this.route("team", { path: "/team" });
+      this.route("billing", { path: "/billing" });
+      this.route("edit", { path: "/:id" });
     });
 
     //
     // Bills
-    this.route('bills', {resetNamespace: true}, function() {
-      this.route('detail', {path: '/:id'});
-      this.route('saved');
-      this.route('add');
-      this.route('search');
-      this.route('comments', {path: '/:id/comments'});
-      this.route('notes', {path: '/:id/notes'});
-      this.route('share', {path: '/:id/share'});
-      this.route('source', {path: '/:id/source'});
+    this.route("bills", { resetNamespace: true }, function() {
+      this.route("detail", { path: "/:id" });
+      this.route("saved");
+      this.route("add");
+      this.route("search");
+      this.route("comments", { path: "/:id/comments" });
+      this.route("notes", { path: "/:id/notes" });
+      this.route("share", { path: "/:id/share" });
+      this.route("source", { path: "/:id/source" });
     });
 
     //
     // Groups
-    this.route('groups', {resetNamespace: true}, function() {
-      this.route('detail', {path: '/:id'});
-      this.route('edit', {path: '/:id/edit'});
-      this.route('add');
-      this.route('contacts', {path: '/:id/contacts'});
-      this.route('bills', {path: '/:id/bills'});
-      this.route('message', {path: '/:id/message'});
+    this.route("groups", { resetNamespace: true }, function() {
+      this.route("detail", { path: "/:id" });
+      this.route("edit", { path: "/:id/edit" });
+      this.route("add");
+      this.route("contacts", { path: "/:id/contacts" });
+      this.route("bills", { path: "/:id/bills" });
+      this.route("message", { path: "/:id/message" });
     });
 
     //
     // Reports
-    this.route('reports', {path: '/:group/reports', resetNamespace: true}, function () {
-      this.route('edit', {path: '/:report/edit'});
-      this.route('add', {path: '/add'});
-      this.route('detail', {path: '/:report'});
-    });
+    this.route(
+      "reports",
+      { path: "/:group/reports", resetNamespace: true },
+      function() {
+        this.route("edit", { path: "/:report/edit" });
+        this.route("add", { path: "/add" });
+        this.route("detail", { path: "/:report" });
+      }
+    );
   });
-
 
   //
   // Administration
-  this.route('admin', function() {
-    this.route('organizations', function() {
-      this.route('detail');
+  this.route("admin", function() {
+    this.route("organizations", function() {
+      this.route("detail");
     });
 
-    this.route('users', function() {
-      this.route('detail');
+    this.route("users", function() {
+      this.route("detail");
     });
   });
-
 });
 
 export default Router;
