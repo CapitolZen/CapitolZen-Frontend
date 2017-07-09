b349cc61540584a00a41090a4a1c4f13961ea5c3
Donald Wasserman <djwasserman@gmail.com>
2017-07-09 06:46:59 -0400

    added filter routes to groups

--- app/components/ui/group-nav.js
+++ app/components/ui/group-nav.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
-  classNames: ['group-nav-wrapper']
+  classNames: ["group-nav-wrapper", "mw-100"]
 });
