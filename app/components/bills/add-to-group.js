2715dcd1771740a92d5ba3096eead42688dfe079
Donald Wasserman <djwasserman@gmail.com>
2017-08-02 16:29:37 -0400

    more bill table fixes

--- app/components/bills/add-to-group.js
+++ app/components/bills/add-to-group.js
@@ -1,7 +1,7 @@
 import Ember from "ember";
-import { task } from "ember-concurrency";
+import { task, hash } from "ember-concurrency";
 
-const { inject: { service }, get, set } = Ember;
+const { inject: { service }, get, set, RSVP } = Ember;
 
 export default Ember.Component.extend({
   store: service(),
@@ -15,15 +15,46 @@ export default Ember.Component.extend({
   buttonType: "secondary",
   menuAlign: "left",
   listGroups: task(function*() {
-    let groups = yield get(this, "store").findAll("group");
-    set(this, "groupList", groups);
+    let bill = get(this, "bill");
+
+    let storeHash = {
+      wrappers: get(this, "store").query("wrapper", {
+        bill__state_id: bill.get("stateId"),
+        bill__state: bill.get("state")
+      }),
+      groups: get(this, "store").findAll("group")
+    };
+
+    let { wrappers, groups } = yield hash(storeHash);
+
+    let wrapperGroupIds = wrappers.map(w => {
+      return get(w, "group.id");
+    });
+
+    if (wrapperGroupIds.length && groups.length) {
+      let filteredList = [];
+      groups.forEach(g => {
+        if (wrapperGroupIds.indexOf(g.get("id")) < 0) {
+          filteredList.push(g);
+        }
+      });
+      console.log(filteredList);
+      set(this, "groupList", filteredList);
+    } else {
+      console.log("sup");
+      set(this, "groupList", groups);
+    }
   }),
   addBillToGroup: task(function*(group) {
     let bill = get(this, "bill");
+    console.log(group);
+    console.log(group.get("id"));
     let wrapper = yield this.get("store").query("wrapper", {
       bill__state_id: bill.get("stateId"),
-      bill__state: bill.get("state")
+      bill__state: bill.get("state"),
+      group: group.get("id")
     });
+
     if (!wrapper.get("length")) {
       wrapper = this.get("store").createRecord("wrapper", {
         bill: get(this, "bill"),
