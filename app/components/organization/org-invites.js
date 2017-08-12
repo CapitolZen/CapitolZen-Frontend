41fbcb76493743b7972fdb1d8391bc433e274e19
Donald Wasserman <djwasserman@gmail.com>
2017-08-12 08:24:10 -0400

    adding invite actions

--- app/components/organization/org-invites.js
+++ app/components/organization/org-invites.js
@@ -10,7 +10,27 @@ export default Component.extend({
   inviteEmail: null,
   inviteSortDesc: ["name:desc"],
   sortedInvites: computed.sort("invites", "inviteSortDesc"),
-  revokeInvite: task(function*(invite) {}),
+  revokeInvite: task(function*(invite) {
+    let res = yield get(this, "request").post(`${invite.get("id")}/actions/`, {
+      data: {
+        actions: "revoke"
+      }
+    });
+
+    invite.set("status", "revoked");
+    get(this, "flashMessages").success(
+      `Invite to ${invite.get("email")} is revoked.`
+    );
+  }),
+  resendInvite: task(function*(invite) {
+    let res = yield get(this, "request").post(`${invite.get("id")}/actions/`, {
+      data: {
+        actions: "resend"
+      }
+    });
+
+    get(this, "flashMessages").success("Resent Invite");
+  }).drop(),
   actions: {
     inviteUser() {
       let invite = get(this, "store").createRecord("invite", {
