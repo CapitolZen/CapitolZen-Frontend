fac13cf1a23aed376cfca5c5786f041b8d8ef226
Matt Wisner <matt@detroitweb.company>
2017-08-20 21:31:56 -0400

    normalize responses

--- app/serializers/activity.js
+++ app/serializers/activity.js
@@ -1,3 +1,19 @@
 import DS from 'ember-data';
 
-export default DS.JSONSerializer.extend({});
+export default DS.JSONSerializer.extend({
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    return this._super(...arguments);
+  },
+
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
+    let actual_payload = payload;
+
+    payload = [];
+
+    for (let result of actual_payload['results']) {
+      payload = payload.concat(result['activities']);
+    }
+
+    return this._super(store, primaryModelClass, payload, id, requestType);
+  }
+});
