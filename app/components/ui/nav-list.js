3e1fa4eb8ce272f6a83eab4bb1ac2c43f6e130b2
Donald Wasserman <djwasserman@gmail.com>
2017-09-28 17:43:32 -0400

    latest bills (#58)
    * ui munging
    
    * latest bills

--- app/components/ui/nav-list.js
+++ app/components/ui/nav-list.js
@@ -1,10 +1,15 @@
 import Component from '@ember/component';
 import { get } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  session: service(),
   actions: {
     triggerLink() {
       get(this, 'dismiss')();
+    },
+    invalidateSession() {
+      get(this, 'session').invalidate();
     }
   }
 });
--- app/controllers/app.js
+++ app/controllers/app.js
@@ -4,14 +4,10 @@ import { inject as service } from '@ember/service';
 import { get, set } from '@ember/object';
 
 export default Controller.extend({
-  session: service('session'),
   currentUser: service('current-user'),
   menuState: false,
   locked: false,
   actions: {
-    invalidateSession() {
-      this.get('session').invalidate();
-    },
     toggleMenuState() {
       set(this, 'menuState', true);
     }
