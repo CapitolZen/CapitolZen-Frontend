a9480510c31fc28a16835b1c6ffd57e6af2327cb
Matt Wisner <matt@detroitweb.company>
2017-10-29 11:33:13 -0400

    fixing unsaved client issue, some bill search fixes

--- app/components/groups/group-form.js
+++ app/components/groups/group-form.js
@@ -18,14 +18,7 @@ export default FormComponent.extend({
    * Model setup
    */
   initModel() {
-    if (this.get('group')) {
-      this.set('model', this.get('group'));
-    } else {
-      this.set(
-        'model',
-        this.get('store').createRecord('group', { active: true })
-      );
-    }
+    this.set('model', this.get('group'));
 
     let changeset = new Changeset(
       this.get('model'),
@@ -49,5 +42,17 @@ export default FormComponent.extend({
 
   actions: {
     saveGroupToUser() {}
+  },
+
+  /**
+   * Automatically destroy any models that were created and then left around.
+   * TODO: can we detect if model is a pojo or a ember model object?
+   * if so we should move this to the main base-form.
+   */
+  willDestroyElement() {
+    this._super(...arguments);
+    if (!this.get('model.id')) {
+      this.get('model').deleteRecord();
+    }
   }
 });
