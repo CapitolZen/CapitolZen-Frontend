0368d4253bf6127640ccbe66b7f83dfcbdd3cd7b
Donald Wasserman <djwasserman@gmail.com>
2017-10-31 15:36:05 -0400

    misc group/report munging

--- app/components/groups/group-form.js
+++ app/components/groups/group-form.js
@@ -18,7 +18,9 @@ export default FormComponent.extend({
    * Model setup
    */
   initModel() {
-    this.set('model', this.get('group'));
+    let group = get(this, 'group');
+    set(group, 'organization', get(this, 'organization'));
+    set(this, 'model', group);
 
     let changeset = new Changeset(
       this.get('model'),
@@ -33,6 +35,7 @@ export default FormComponent.extend({
    */
   onSubmitSuccess() {
     get(this, 'flashMessages').success('Client Saved');
+    get(this, 'router').transitionTo('groups.index');
   },
 
   /**
@@ -42,17 +45,5 @@ export default FormComponent.extend({
 
   actions: {
     saveGroupToUser() {}
-  },
-
-  /**
-   * Automatically destroy any models that were created and then left around.
-   * TODO: can we detect if model is a pojo or a ember model object?
-   * if so we should move this to the main base-form.
-   */
-  willDestroyElement() {
-    this._super(...arguments);
-    if (!this.get('model.id')) {
-      this.get('model').deleteRecord();
-    }
   }
 });
