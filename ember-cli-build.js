4c104c0ebe419156f0eb8ea77f0c4ebea0032f59
Matt Wisner <matt@detroitweb.company>
2017-11-10 10:48:02 -0500

    Attempting to fix build errors

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -25,7 +25,6 @@ module.exports = function(defaults) {
     'ember-power-select': {
       theme: 'bootstrap'
     },
-    'ember-junkdrawer': {},
     sassOptions: {
       includePaths: ['vendor', 'node_modules/bootstrap/scss']
     }
