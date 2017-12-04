4af8901d0fcdf84aefffd0e28c9872c36ddce390
Donald Wasserman <djwasserman@gmail.com>
2017-12-04 14:23:02 -0500

    added some basic loading stuff for registration page

--- app/components/anon/user-register.js
+++ app/components/anon/user-register.js
@@ -1,7 +1,7 @@
 import { inject as service } from '@ember/service';
 import { computed, get, set } from '@ember/object';
 import UserRegistration from '../../validators/user-registration';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 import FormComponent from 'ember-junkdrawer/components/form/changeset-form';
 
 export default FormComponent.extend({
@@ -24,13 +24,27 @@ export default FormComponent.extend({
       password: this.get('model.password')
     });
   },
+  setupStep: null,
+  generateFakeSteps: task(function*() {
+    set(this, 'setupStep', 'Creating Account');
+    yield timeout(1500);
+    set(this, 'setupStep', 'Creating Organization');
+    yield timeout(1500);
+    set(this, 'setupStep', 'Finding Committee Meetings');
+    yield timeout(1500);
+    set(this, 'setupStep', 'Gathering Bills');
+    yield timeout(1500);
+    set(this, 'setupStep', 'Loading Application');
+    yield timeout(1500);
+    set(this, 'setupStep', 'Reticulating Splines');
+  }),
 
   /**
    * Replace the submit handler since we're not just running changeset.save
    */
   submit: task(function*(changeset) {
     changeset.execute();
-
+    get(this, 'generateFakeSteps').perform();
     yield this.get('ajax')
       .post('users/register/', { data: this.get('model') })
       .then(data => {
