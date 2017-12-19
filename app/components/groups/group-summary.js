ab388d2a01d25e47c6142b19ddd8f60c6b1c6166
Matt Wisner <matt@detroitweb.company>
2017-12-19 00:44:07 -0500

    Roughing in a new client  / list page concepts. New magical ember avatar feature added

--- app/components/groups/group-summary.js
+++ app/components/groups/group-summary.js
@@ -3,12 +3,11 @@ import { get, computed } from '@ember/object';
 export default Component.extend({
   classNames: ['my-1'],
   classNameBindings: ['sizeClass'],
-  sizeClass: computed('size', function() {
-    let size = get(this, 'size') || 'half';
-    if (size === 'full') {
-      return 'col-12';
-    } else {
-      return 'col-lg-6';
-    }
+  sizeClass: computed('presentation', 'size', function() {
+    let classes = 'group-summary';
+
+    classes += ' group-' + this.get('presentation');
+
+    return classes;
   })
 });
