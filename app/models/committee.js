d97c5e133f678000b05e9e88c3e5ebf59f032482
Donald Wasserman <djwasserman@gmail.com>
2018-01-10 13:45:41 -0500

    add front end subscriptions

--- app/models/committee.js
+++ app/models/committee.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 import { computed, get } from '@ember/object';
+import { chamberName } from '../helpers/chamber-name';
+
 export default DS.Model.extend({
   bills: DS.hasMany('bill'),
   modified: DS.attr('string'),
@@ -11,7 +13,10 @@ export default DS.Model.extend({
   subcommittee: DS.attr('string'),
   meetings: DS.hasMany('event'),
   displayName: computed('name', 'subcommittee', function() {
-    let name = get(this, 'name');
+    let name = get(this, 'name'),
+      chamber = chamberName([get(this, 'chamber')]);
+
+    name = `${chamber} ${name}`;
     if (get(this, 'subcommittee')) {
       name += `: ${get(this, 'subcommittee')}`;
     }
