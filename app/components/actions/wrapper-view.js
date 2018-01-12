a2848111e2ba8cd71593091b841158746f066ed4
Donald Wasserman <djwasserman@gmail.com>
2018-01-12 14:53:52 -0500

    wrapper detail

--- app/components/actions/wrapper-view.js
+++ app/components/actions/wrapper-view.js
@@ -1,4 +1,14 @@
-import Component from '@ember/component';
-import { get, set, computed } from '@ember/object';
+import Base from './view-base';
+import { get, getWithDefault, set, computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
+import { task } from 'ember-concurrency';
 
-export default Component.extend({});
+export default Base.extend({
+  displayTitle: 'Bill Updated',
+  dismiss({ length }) {},
+  hasCommittee: computed(function() {
+    return getWithDefault(this, 'referencedModel.metadata.committee-id', false);
+  }),
+  referencedCommittee: false,
+  fetchCommittee: task(function*() {})
+});
