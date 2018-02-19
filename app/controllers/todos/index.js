347e8b2830556db1c3623d74c53f834c32bcf7e1
Donald Wasserman <djwasserman@gmail.com>
2018-02-18 21:05:17 -0500

    bump packages + page title on todos

--- app/controllers/todos/index.js
+++ app/controllers/todos/index.js
@@ -1,7 +1,19 @@
 import Controller from '@ember/controller';
+import { computed, get } from '@ember/object';
 
 export default Controller.extend({
   queryParams: ['group', 'type'],
   group: null,
-  type: null
+  type: null,
+  pageTitle: computed('group', 'type', function() {
+    if (get(this, 'type') === 'wrapper:updated' || get(this, 'group')) {
+      return 'Saved Bill';
+    }
+
+    if (get(this, 'type') === 'bill:introduced') {
+      return 'Introduced Bill';
+    }
+
+    return '';
+  })
 });
