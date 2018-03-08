44ba6d50d35118604f5f7a2f74aeb12a436ba82f
Donald Wasserman <djwasserman@gmail.com>
2018-03-08 14:32:01 -0500

    add group contact user management

---
+++ app/components/groups/-users/list.js
@@ -0,0 +1,46 @@
+import Component from '@ember/component';
+import { get, set, computed } from '@ember/object';
+import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import { A } from '@ember/array';
+
+export default Component.extend({
+  store: service(),
+  guests: A(),
+  init() {
+    this._super(...arguments);
+    get(this, 'loadGuests').perform();
+  },
+  loadGuests: task(function*() {
+    let guests = yield get(this, 'store').query('user', {
+      guest: get(this, 'group.id'),
+      is_active: true
+    });
+
+    get(this, 'guests').addObjects(guests);
+  }),
+  deleteUser: task(function*(user) {
+    user.set('isActive', false);
+    yield user.save();
+
+    let payload = {
+      data: {
+        type: 'users',
+        attributes: {
+          status: status
+        }
+      }
+    };
+
+    user.change_status(payload);
+
+    get(this, 'guests').removeObject(user);
+  }),
+  actions: {
+    submit(data) {
+      set(this, 'modal', false);
+      let model = get(this, 'store').push(data);
+      get(this, 'guests').addObject(model);
+    }
+  }
+});
