299e3710d2f7ad31e436829df13670bd412b020d
Donald Wasserman <djwasserman@gmail.com>
2018-03-19 11:33:56 -0400

    added some better visbility on the page layout

--- app/components/pages/-anon/display.js
+++ app/components/pages/-anon/display.js
@@ -8,14 +8,22 @@ export default Component.extend({
   store: service(),
   updateList: A(),
   classNames: ['mt-3'],
+  currentPage: 0,
+  totalPages: 1,
   init() {
     this._super(...arguments);
     get(this, 'loadUpdates').perform();
+    let cachedUpdates = this.get('store').peekAll('update');
+    this.get('updateList').addObjects(cachedUpdates);
   },
   loadUpdates: task(function*() {
     let group_page = get(this, 'page.id');
-    let updates = yield get(this, 'store').query('update', { group_page });
-    console.log(updates);
-    get(this, 'updateList').addObjects(updates);
+    if (this.get('currentPage') < this.get('totalPages')) {
+      let updates = yield get(this, 'store').query('update', { group_page });
+      let { page, pages } = updates.get('meta.pagination');
+      set(this, 'currentPage', page);
+      set(this, 'totalPages', pages);
+      get(this, 'updateList').addObjects(updates);
+    }
   })
 });
