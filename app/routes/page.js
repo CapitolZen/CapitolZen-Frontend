3eb7571ecbf251efbed9fde11aca9fd991c7bca3
Donald Wasserman <djwasserman@gmail.com>
2018-04-13 16:06:53 -0400

    teardown logic

--- app/routes/page.js
+++ app/routes/page.js
@@ -1,13 +1,17 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { set } from '@ember/object';
+import { on } from '@ember/object/evented';
 
 export default Route.extend({
   session: service(),
 
   beforeModel(transition) {
     let { params } = transition;
-
+    let id = this.getIdFromChild(params);
+    set(this, 'session.data.currentPageId', id);
+  },
+  getIdFromChild(params) {
     let allowedRoutes = [
       'page.updates',
       'page.update',
@@ -16,25 +20,18 @@ export default Route.extend({
     ];
     let id;
     allowedRoutes.forEach(r => {
-      console.log(r);
       if (params.hasOwnProperty(r)) {
         id = params[r].id;
       }
     });
-
-    set(this, 'session.data.currentPageId', id);
+    return id;
   },
+  unsetCurrentPage: on('deactivate', function() {
+    set(this, 'session.data.currentPageId', false);
+  }),
   actions: {
     error(err, { params }) {
-      let id;
-      if (params.hasOwnProperty('page.updates')) {
-        id = params['page.updates'];
-      }
-
-      if (params.hasOwnProperty('page.update')) {
-        id = params['page.update'];
-      }
-
+      let id = this.getIdFromChild(params);
       this.replaceWith('anon.page-access', id);
     }
   }
